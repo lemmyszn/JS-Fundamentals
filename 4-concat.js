fedc990d69c7ca9a5f609a968767831c54230d4a
lemmyszn <lemmyxking17@gmail.com>
2025-05-21 02:18:13 -0700

    Add script to concatenate two command line arguments

---
+++ 4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+// Get the two arguments
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print the arguments in the required format
+console.log(firstArg + " is " + secondArg);
